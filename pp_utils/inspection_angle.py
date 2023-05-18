87988cfbddaacfb67bb0660d24f8443edb5b9538
Wu-Jung Lee <leewujung@gmail.com>
2023-05-18 16:40:25 -0700

    add fig echo angle TS notebook, add note re no explicit trial selection in notebooks

--- pp_utils/inspection_angle.py
+++ pp_utils/inspection_angle.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import xarray as xr
 
 
 def get_time_corrected_bins(df_h: pd.DataFrame, bin_delta=0.05):
@@ -50,23 +51,24 @@ def groupby_ops(df_h: pd.DataFrame, col_name: str, time_corrected_bins: pd.cut, 
     return s.to_frame().dropna()
 
 
-def get_inspection_angle_dist(
-    df_ch0,
-    df_ch1,
+def get_angle_dist(
+    df_click,
     col_name,
-    enso_bin=np.arange(0, 365, 5),
+    angle_bin=np.arange(0, 365, 5),
     time_binning=False,
     count_type="median",
-    bin_delta=0.02,
+    bin_delta=None,
     density=False,
 ):
     """
+    Get angle
+
     Parameters
     ----------
-    df_ch0 / df_ch1 : pd.DataFrame
+    df_click : pd.DataFrame
         filtered dataframes containing hydrophoen click detections to be used
         to figure out distribution
-    enso_bin : np.ndarray
+    angle_bin : np.ndarray
         bins for ensonification angle
     col_name : str
         name of column to compute mean or median
@@ -85,24 +87,23 @@ def get_inspection_angle_dist(
 
     Returns
     -------
-    ch0_dist, ch1_dist
-        numpy arrays containing the click counts along the input vector enso_bin
+    A numpy array containing the click counts along the input vector enso_bin
     """
+    # Check input combination
+    if time_binning is False and bin_delta is not None:
+        raise ValueError("bin_delta cannot be used when time_binning is False!")
 
-    def proc_ch(x):
-        # if input is empty
-        if x is None or len(x) == 0:
-            return np.zeros(len(enso_bin) - 1)
-        else:
-            if time_binning:  # count clicks by interval
-                tc_bins = get_time_corrected_bins(df_h=x, bin_delta=bin_delta)
-                enso_angle = groupby_ops(x, col_name, tc_bins, count_type).values
-                out, _ = np.histogram(enso_angle, bins=enso_bin, density=density)
-            else:  # count clicks as is
-                out, _ = np.histogram(x[col_name], bins=enso_bin, density=density)
-            return out
-
-    return proc_ch(df_ch0), proc_ch(df_ch1)
+    # if input is empty
+    if df_click is None or len(df_click) == 0:
+        return np.zeros(len(angle_bin) - 1)
+    else:
+        if time_binning:  # count clicks by interval
+            tc_bins = get_time_corrected_bins(df_h=df_click, bin_delta=bin_delta)
+            enso_angle = groupby_ops(df_click, col_name, tc_bins, count_type).values
+            out, _ = np.histogram(enso_angle, bins=angle_bin, density=density)
+        else:  # count clicks as is
+            out, _ = np.histogram(df_click[col_name], bins=angle_bin, density=density)
+        return out
 
 
 def enso2spheroid_angle(enso_angle):
@@ -130,3 +131,30 @@ def enso2spheroid_angle(enso_angle):
 
     return sph_echo_angle.squeeze()
 
+
+def get_spheroid_echo(sph_echo_angle, sph_ar, sph_path, sph_file):
+    """
+    Get spheroid echo measurements with angles closest to
+    those in the input sph_echo_angle.
+
+    The input angles are discretizied at 15 deg intervals
+    when selecting the correpsonding echo measurements
+    due to constraints from the experiment (each angle took
+    too long so only measured at 15 deg intervals).
+
+    Parameters
+    ----------
+    sph_echo_angle : float
+        angles used in spheroid echo measurements
+    sph_ar : str
+        aspect ratio of spheroid
+        allowed values: "1.0", "1.3", "2.9"
+
+    Returns
+    -------
+    xr.DataArray
+        An xarray DataArray containing spheroid echo measurements
+        at angles closest to the input angles.
+    """
+    da_sph = xr.open_dataarray(sph_path / sph_file[sph_ar])
+    return da_sph.sel(angle=sph_echo_angle, method="nearest")
